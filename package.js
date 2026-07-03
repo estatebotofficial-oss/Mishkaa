5e4f565329867a5299787077e6333302a26d631a
estatebotofficial-oss <estatebotofficial@gmail.com>
2026-07-03 22:38:49 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "mishkaa-sub-backend",
+  "version": "1.0.0",
+  "description": "Automatic Cashfree Subscription + Telegram group access backend for Mishkaa",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "dotenv": "^16.4.5",
+    "@supabase/supabase-js": "^2.45.0",
+    "node-fetch": "^2.7.0"
+  }
+}
